6b6381bca311a19881e37b303b96f57c993a58a5
Abhinav Kumar <89967724+Abhinav-6@users.noreply.github.com>
2022-05-19 12:11:20 +0530

    making nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,14 +11,24 @@ export default function Header() {
       </Logo>
       <NavBar>
         <nav>
-          <ul>
-            <li>Model S</li>
-            <li>Model 3</li>
-            <li>Model X</li>
-            <li>Model Y</li>
-            <li>Solar Roof</li>
-            <li>Solar Panels</li>
-          </ul>
+          <li>
+            <a href="#">Model S</a>
+          </li>
+          <li>
+            <a href="#">Model 3</a>
+          </li>
+          <li>
+            <a href="#">Model X</a>
+          </li>
+          <li>
+            <a href="#">Model Y</a>
+          </li>
+          <li>
+            <a href="#">Solar Roof</a>
+          </li>
+          <li>
+            <a href="#">Solar Panels</a>
+          </li>
         </nav>
       </NavBar>
     </Container>
@@ -27,20 +37,46 @@ export default function Header() {
 
 const Container = styled.div`
   height: 56px;
+  width: 100%;
   background: transparent;
   position: fixed;
   padding: 0 32px;
   display: flex;
+  justify-content: space-between;
 `;
 
 const Logo = styled.div`
   height: 100%;
   display: flex;
   align-items: center;
+  /* margin-left: auto; */
 `;
 
 const LogoImg = styled.img`
   line-height: 40px;
 `;
 
-const NavBar = styled.div``;
+const NavBar = styled.div`
+  font-family: "Roboto Flex", sans-serif;
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 1024px) {
+    display: none;
+  }
+  li {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 91px;
+    height: 30px;
+    padding: auto 10px;
+    font-size: 14px;
+    font-weight: 700;
+    fill: #393c41;
+  }
+  li:hover{
+    /* color: black; */
+    background-color: 
+  }
+`;
